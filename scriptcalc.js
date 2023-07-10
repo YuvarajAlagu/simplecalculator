784bdb380c33b40b4fe8269556db7d1bbdd277d1
YuvarajAlagu <136988264+YuvarajAlagu@users.noreply.github.com>
2023-07-10 21:01:45 +0530

    Add files via upload
    support n number of inputs

--- scriptcalc.js
+++ scriptcalc.js
@@ -1,10 +1,17 @@
 const button = document.querySelector("#submit");
 const button1 = document.getElementById("clear");
+const button2 = document.getElementById("clearAll");
 button.addEventListener('click',convert);
 button1.addEventListener('click',() => {
-    document.getElementById("input").value='';
+    let cl = document.getElementById("input").value;
+    let a = cl.slice(0,-1);
+    document.getElementById("input").value=a;
 });
-function convert(){
+button2.addEventListener('click',()=>{
+    document.getElementById("input").value='';
+    result.innerHTML='';
+});
+/*function convert(){
     let num = document.getElementById("input").value;
     let result = document.querySelector("#result");
     let split = '';
@@ -38,5 +45,34 @@ else if(num.includes('%')){
 else{
     s='Input format is wrong';
     result.innerHTML=s;
+}*/
+function convert(){
+    let num = document.getElementById("input").value;
+    let result = document.querySelector("#result");
+    let split = '';
+    let s=0,j=0,n='',sym='';
+    for(let i=0;i<num.length;i++){
+    if(num[i] == '+' || num[i] == '-' || num[i] == '*' || num[i] == '/' || num[i] == '%'){
+        sym += num[i];
+    }
 }
+split = num.split(/[+\-*/%]/);
+for(let j=0;j<split.length;j++){
+    if(sym[j] == '+'){
+        split[j+1] = Number(split[j]) + Number(split[j+1]);
+    }
+    if(sym[j] == '-'){
+        split[j+1] = Number(split[j]) - Number(split[j+1]);
+    }
+    if(sym[j] == '*'){
+        split[j+1] = Number(split[j]) * Number(split[j+1]);
+    }
+    if(sym[j] == '/'){
+        split[j+1] = Number(split[j]) * Number(split[j+1]);
+    }
+    if(sym[j] == '%'){
+        split[j+1] = Number(split[j]) % Number(split[j+1]);
+    }
+}
+result.innerHTML="The Answer Is  " + split[split.length-1];
 }
